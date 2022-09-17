43656aeacafa17b62ea0cc34b8761b06699a46d8
adamhatton <adamdhatton@gmail.com>
2022-09-17 13:46:43 +0000

    Add code from videos

--- src/components/RenderingLists.js
+++ src/components/RenderingLists.js
@@ -29,13 +29,13 @@ const books = [
   return (
     <div>
         {bookList.map(book => {
-            return <h2>{book}</h2>
+            return <h2 key={book}>{book}</h2>
         })}
         <hr />
         {
             books.map(book => {
                 return(
-                    <Book book={book}/>
+                    <Book key={book.title} book={book}/>
                 )
             })
         }
